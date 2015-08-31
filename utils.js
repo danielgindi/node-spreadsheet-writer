f4b7fdb1914ce74b1b2ef0bba1d9c31dbe845db8
Daniel Cohen Gindi <Danielgindi@gmail.com>
2015-08-31 10:34:25 +0300

    Converter for date formats to Excel

--- utils.js
+++ utils.js
@@ -74,6 +74,63 @@ var Utils = {
 
     })()
 
+    , excelNumberFormatForDateFormat: (function () {
+
+        var formatMatcher = /d{1,4}|M{1,4}|yy(?:yy)?|([HhmsTt])\1?|[LloSZ]|UTC|"[^"]*"|'[^']*'|[ .0#%,Ee\\"']/g;
+
+        // https://msdn.microsoft.com/en-us/library/office/gg251755.aspx
+        var flagMap = {
+            'd': 'd',
+            'dd': 'dd',
+            'ddd': 'ddd',
+            'dddd': 'aaaa', // dddd is English, aaaa is localized
+            'M': 'm',
+            'MM': 'mm',
+            'MMM': 'mmm',
+            'MMMM': 'oooo', // mmmm is English, oooo is localized
+            'yy': 'yy',
+            'yyyy': 'yyyy',
+            'h': 'h',
+            'hh': 'Hh',
+            'H': 'h', // 12/24 hours automatic depending on existence of AM/PM
+            'HH': 'Hh',
+            'm': 'N',
+            'mm': 'Nn',
+            's': 'S',
+            'ss': 'Ss',
+            'l': '"000"', // No milliseconds in Excel
+            'L': '"00"', // No milliseconds in Excel
+            't': 'a/p',
+            'tt': 'am/pm',
+            'T': 'A/P',
+            'TT': 'AM/PM',
+            'Z': '', // No access to timezone data
+            'UTC': '', // No access to timezone data
+            'o': '', // No access to timezone data
+            'S': '', // No such feature in Excel
+            '"': '\\"',
+            '\\': '\\\\'
+        };
+        
+        var replacer = function (token) {
+            console.log(token)
+            return flagMap.hasOwnProperty(token) ?
+                flagMap[token] :
+                ('"' +
+                ((token.length > 1 && (token[0] === "'" || token[0] === '"') && token[token.length - 1] === token[0]) ?
+                    token.slice(1, token.length - 1) :
+                    token) +
+                '"');
+        };
+        
+        return function (format) {
+
+            return format.replace(formatMatcher, replacer);
+
+        };
+
+    })()
+
     /*, mkdirpMulti: function (dirs, mode, callback) {
         if (typeof mode === 'function') {
             callback = /* * @type {function} * / mode;
